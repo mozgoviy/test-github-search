3c88706e141ee13705f7c3c90b568cd395f168dd
s.mozghovoy <you@example.com>
2017-06-21 18:26:26 +0300

    initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# platform :ios, ’10.0’
+
+use_frameworks!
+
+target 'test-mozghovoy' do
+  
+  pod 'Alamofire', '~> 4.4'
+  pod 'SwiftyJSON', '~> 3.1.4'
+
+end
---
+++ test-mozghovoy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-mozghovoy
+//
+//  Created by Sergey Mozgovoy on 21/06/2017.
+//  Copyright © 2017 Sergey Mozgovoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
